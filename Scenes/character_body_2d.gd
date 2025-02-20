4d1cf28fe31f5f3dff3c92e4a33b248838d99425
vitvitcher <79949751+vitvitcher@users.noreply.github.com>
2025-02-20 17:36:38 +0200

    character movement

--- Scenes/character_body_2d.gd
+++ Scenes/character_body_2d.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-
+@onready var sprite:AnimatedSprite2D = $CharacterSprite
 
 const SPEED = 300.0
 
@@ -13,8 +13,17 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.y = direction.y * SPEED
+		if velocity.y<0:
+			sprite.play("walk_up")
+		elif velocity.y>0:
+			sprite.play("walk_down")
+		
+		if velocity.x>0:
+			sprite.play("walk_right")
+		elif velocity.x<0:
+			sprite.play("walk_left")
+		
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED*delta)
-		velocity.y = move_toward(velocity.y, 0, SPEED*delta)
-
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 	move_and_slide()
