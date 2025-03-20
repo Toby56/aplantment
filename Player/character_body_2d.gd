a44d180f608d9d7446291aecaab0dfa17efb8b92
vitvitcher <79949751+vitvitcher@users.noreply.github.com>
2025-03-20 10:05:26 +0200

    basic inventory functionality
    left click - take or put everything/swap items
    right click - take half/incert 1 of item
    Enter to test inserting items

--- Player/character_body_2d.gd
+++ Player/character_body_2d.gd
@@ -11,7 +11,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	var  direction:= Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	if direction:
 		velocity = Vector2(direction.x,direction.y).normalized()*SPEED
 		
